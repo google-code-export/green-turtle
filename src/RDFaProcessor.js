2f322d0673040cec65c66e8ea55276f416477696
alex@milowski.com <alex@milowski.com@0d82c3c9-fcea-40e7-27d5-4b35ae388c16>
2012-03-01 01:35:52 +0000

    simplified RDFaProcessor by removing external dependencies

--- src/RDFaProcessor.js
+++ src/RDFaProcessor.js
@@ -1,6 +1,7 @@
 
 function RDFaProcessor(targetObject) {
    this.target = targetObject;
+   this.target.tripleCount = 0;
    this.target.triplesGraph = {};
    this.target.subjectOrigins = {};
    this.prefixes = {};
@@ -12,8 +13,12 @@ function RDFaProcessor(targetObject) {
    this.XMLLiteralURI = "http://www.w3.org/1999/02/22-rdf-syntax-ns#XMLLiteral"; 
    this.PlainLiteralURI = "http://www.w3.org/1999/02/22-rdf-syntax-ns#PlainLiteral";
    this.blankCounter = 0;
-   this.curieParser = new CurieParser();
 }
+
+RDFaProcessor.SCHEME = new RegExp("^[A-Za-z][A-Za-z0-9\+\-\.]*\:");
+RDFaProcessor.nameChar = '[-A-Z_a-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\u10000-\uEFFFF\.0-9\u00B7\u0300-\u036F\u203F-\u2040]';
+RDFaProcessor.nameStartChar = '[\u0041-\u005A\u0061-\u007A\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u00FF\u0100-\u0131\u0134-\u013E\u0141-\u0148\u014A-\u017E\u0180-\u01C3\u01CD-\u01F0\u01F4-\u01F5\u01FA-\u0217\u0250-\u02A8\u02BB-\u02C1\u0386\u0388-\u038A\u038C\u038E-\u03A1\u03A3-\u03CE\u03D0-\u03D6\u03DA\u03DC\u03DE\u03E0\u03E2-\u03F3\u0401-\u040C\u040E-\u044F\u0451-\u045C\u045E-\u0481\u0490-\u04C4\u04C7-\u04C8\u04CB-\u04CC\u04D0-\u04EB\u04EE-\u04F5\u04F8-\u04F9\u0531-\u0556\u0559\u0561-\u0586\u05D0-\u05EA\u05F0-\u05F2\u0621-\u063A\u0641-\u064A\u0671-\u06B7\u06BA-\u06BE\u06C0-\u06CE\u06D0-\u06D3\u06D5\u06E5-\u06E6\u0905-\u0939\u093D\u0958-\u0961\u0985-\u098C\u098F-\u0990\u0993-\u09A8\u09AA-\u09B0\u09B2\u09B6-\u09B9\u09DC-\u09DD\u09DF-\u09E1\u09F0-\u09F1\u0A05-\u0A0A\u0A0F-\u0A10\u0A13-\u0A28\u0A2A-\u0A30\u0A32-\u0A33\u0A35-\u0A36\u0A38-\u0A39\u0A59-\u0A5C\u0A5E\u0A72-\u0A74\u0A85-\u0A8B\u0A8D\u0A8F-\u0A91\u0A93-\u0AA8\u0AAA-\u0AB0\u0AB2-\u0AB3\u0AB5-\u0AB9\u0ABD\u0AE0\u0B05-\u0B0C\u0B0F-\u0B10\u0B13-\u0B28\u0B2A-\u0B30\u0B32-\u0B33\u0B36-\u0B39\u0B3D\u0B5C-\u0B5D\u0B5F-\u0B61\u0B85-\u0B8A\u0B8E-\u0B90\u0B92-\u0B95\u0B99-\u0B9A\u0B9C\u0B9E-\u0B9F\u0BA3-\u0BA4\u0BA8-\u0BAA\u0BAE-\u0BB5\u0BB7-\u0BB9\u0C05-\u0C0C\u0C0E-\u0C10\u0C12-\u0C28\u0C2A-\u0C33\u0C35-\u0C39\u0C60-\u0C61\u0C85-\u0C8C\u0C8E-\u0C90\u0C92-\u0CA8\u0CAA-\u0CB3\u0CB5-\u0CB9\u0CDE\u0CE0-\u0CE1\u0D05-\u0D0C\u0D0E-\u0D10\u0D12-\u0D28\u0D2A-\u0D39\u0D60-\u0D61\u0E01-\u0E2E\u0E30\u0E32-\u0E33\u0E40-\u0E45\u0E81-\u0E82\u0E84\u0E87-\u0E88\u0E8A\u0E8D\u0E94-\u0E97\u0E99-\u0E9F\u0EA1-\u0EA3\u0EA5\u0EA7\u0EAA-\u0EAB\u0EAD-\u0EAE\u0EB0\u0EB2-\u0EB3\u0EBD\u0EC0-\u0EC4\u0F40-\u0F47\u0F49-\u0F69\u10A0-\u10C5\u10D0-\u10F6\u1100\u1102-\u1103\u1105-\u1107\u1109\u110B-\u110C\u110E-\u1112\u113C\u113E\u1140\u114C\u114E\u1150\u1154-\u1155\u1159\u115F-\u1161\u1163\u1165\u1167\u1169\u116D-\u116E\u1172-\u1173\u1175\u119E\u11A8\u11AB\u11AE-\u11AF\u11B7-\u11B8\u11BA\u11BC-\u11C2\u11EB\u11F0\u11F9\u1E00-\u1E9B\u1EA0-\u1EF9\u1F00-\u1F15\u1F18-\u1F1D\u1F20-\u1F45\u1F48-\u1F4D\u1F50-\u1F57\u1F59\u1F5B\u1F5D\u1F5F-\u1F7D\u1F80-\u1FB4\u1FB6-\u1FBC\u1FBE\u1FC2-\u1FC4\u1FC6-\u1FCC\u1FD0-\u1FD3\u1FD6-\u1FDB\u1FE0-\u1FEC\u1FF2-\u1FF4\u1FF6-\u1FFC\u2126\u212A-\u212B\u212E\u2180-\u2182\u3041-\u3094\u30A1-\u30FA\u3105-\u312C\uAC00-\uD7A3\u4E00-\u9FA5\u3007\u3021-\u3029_]';
+RDFaProcessor.NCNAME = new RegExp('^' + RDFaProcessor.nameStartChar + RDFaProcessor.nameChar + '*$');
 
 RDFaProcessor.prototype.trim = function(str) {
    return str.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
@@ -21,6 +26,141 @@ RDFaProcessor.prototype.trim = function(str) {
 
 RDFaProcessor.prototype.tokenize = function(str) {
    return this.trim(str).split(/\s+/);
+}
+
+RDFaProcessor.prototype.parseURI = function(uri) {
+   var match = RDFaProcessor.SCHEME.exec(uri);
+   if (!match) {
+      throw "Bad URI value, no scheme: "+uri;
+   }
+   var parsed = { spec: uri };
+   parsed.scheme = match[0].substring(0,match[0].length-1);
+   parsed.schemeSpecificPart = parsed.spec.substring(match[0].length);
+   if (parsed.schemeSpecificPart.charAt(0)=='/' && parsed.schemeSpecificPart.charAt(1)=='/') {
+      this.parseGeneric(parsed);
+   }
+   var app = this;
+   parsed.resolve = function(href) {
+      if (!href) {
+         return this.spec;
+      }
+      if (!this.isGeneric) {
+         throw "Cannot resolve uri against non-generic URI: "+this.spec;
+      }
+      var colon = href.indexOf(':');
+      if (href.charAt(0)=='/') {
+         return this.scheme+"://"+this.authority+href;
+      } else if (href.charAt(0)=='.' && href.charAt(1)=='/') {
+         if (this.path.charAt(this.path.length-1)=='/') {
+            return this.scheme+"://"+this.authority+this.path+href.substring(2);
+         } else {
+            var last = this.path.lastIndexOf('/');
+            return this.scheme+"://"+this.authority+this.path.substring(0,last)+href.substring(1);
+         }
+      } else if (RDFaProcessor.SCHEME.test(href)) {
+         return href;
+      } else {
+         if (this.path.charAt(this.path.length-1)=='/') {
+            return this.scheme+"://"+this.authority+this.path+href;
+         } else {
+            var last = this.path.lastIndexOf('/');
+            return this.scheme+"://"+this.authority+this.path.substring(0,last+1)+href;
+         }
+      }
+   };
+   return parsed;
+}
+
+RDFaProcessor.prototype.parseGeneric = function(parsed) {
+   if (parsed.schemeSpecificPart.charAt(0)!='/' || parsed.schemeSpecificPart.charAt(1)!='/') {
+      throw "Generic URI values should start with '//':"+parsed.spec;
+   }
+  
+   var work = parsed.schemeSpecificPart.substring(2);
+   var pathStart = work.indexOf("/");
+   parsed.authority = pathStart<0 ? work : work.substring(0,pathStart);
+   parsed.path = pathStart<0 ? "" : work.substring(pathStart);
+   var hash = parsed.path.indexOf('#');
+   if (hash>=0) {
+      parsed.fragment = parsed.path.substring(hash+1);
+      parsed.path = parsed.path.substring(0,hash);
+   }
+   var questionMark = parsed.path.indexOf('?');
+   if (questionMark>=0) {
+      parsed.query = parsed.path.substring(questionMark+1);
+      parsed.path = parsed.path.substring(0,questionMark);
+   }
+   if (parsed.path=="/" || parsed.path=="") {
+      parsed.segments = [];
+   } else {
+      parsed.segments = parsed.path.split(/\//);
+      if (parsed.segments.length>0 && parsed.segments[0]=='' && parsed.path.length>1 && parsed.path.charAt(1)!='/') {
+         // empty segment at the start, remove it
+         parsed.segments.shift();
+      }
+      if (parsed.segments.length>0 && parsed.path.length>0 && parsed.path.charAt(parsed.path.length-1)=='/' && parsed.segments[parsed.segments.length-1]=='') {
+         // we may have an empty the end
+         // check to see if it is legimate
+         if (parsed.path.length>1 && parsed.path.charAt(parsed.path.length-2)!='/') {
+            parsed.segments.pop();
+         }
+      }
+   }
+   parsed.isGeneric = true;
+}
+
+RDFaProcessor.prototype.parseCURIEOrURI = function(value,prefixes,base) {
+   value = this.trim(value);
+   if (value.charAt(0)=='[' && value.charAt(value.length-1)==']') {
+      value = value.substring(1,value.length-1);
+   }
+   var colon = value.indexOf(":");
+   if (colon>=0) {
+      var prefix = value.substring(0,colon);
+      if (prefix=="") {
+         // default prefix
+         var uri = prefixes[""];
+         return uri ? uri+value.substring(colon+1) : null;
+      } else if (prefix=="_") {
+         // blank node
+         return "_:"+value.substring(colon+1);
+      } else if (RDFaProcessor.NCNAME.test(prefix)) {
+         var uri = prefixes[prefix];
+         if (uri) {
+            return uri+value.substring(colon+1);
+         }
+      }
+   }
+   return base.resolve(value);
+}
+
+RDFaProcessor.prototype.parseTermOrCURIEOrURI = function(value,terms,prefixes,base) {
+   value = this.trim(value);
+   if (value.charAt(0)=='[' && value.charAt(value.length-1)==']') {
+      value = value.substring(1,value.length-1);
+   }
+   var term = terms[value];
+   if (term) {
+      return term;
+   }
+   var colon = value.indexOf(":");
+   if (colon>=0) {
+      var prefix = value.substring(0,colon);
+      if (prefix=="") {
+         // default prefix
+         var uri = prefixes[""];
+         return uri ? uri+value.substring(colon+1) : null;
+      } else if (prefix=="_") {
+         // blank node
+         return "_:"+value.substring(colon+1);
+      } else if (RDFaProcessor.NCNAME.test(prefix)) {
+         var uri = prefixes[prefix];
+         if (uri) {
+            return uri+value.substring(colon+1);
+         }
+      }
+   }
+   return base.resolve(value);
 }
 
 RDFaProcessor.prototype.parsePrefixMappings = function(str,target) {
@@ -157,6 +297,7 @@ RDFaProcessor.prototype.addTriple = function(data,origin,subject,predicate,objec
    }
    data.tripleCount++;
    pnode.objects.push(object);
+   //console.log("Added triple.");
 }
 
 RDFaProcessor.prototype.process = function(node) {
@@ -165,12 +306,11 @@ RDFaProcessor.prototype.process = function(node) {
       this.checkForKnownProfiles(node);
    }
    var queue = [];
-   queue.push({ current: node, context: this.push(null,new URI(node.baseURI),node.baseURI)});
+   queue.push({ current: node, context: this.push(null,node.baseURI)});
    while (queue.length>0) {
       var item = queue.shift();
       var current = item.current;
       var context = item.context;
-      //console.log("Processing: "+this.ancestorPath(current));
       var skip = false;
       var newSubject = null;
       var currentObjectResource = null;
@@ -182,7 +322,8 @@ RDFaProcessor.prototype.process = function(node) {
       var termsCopied = false;
       var vocabulary = context.vocabulary;
 
-      var base = new URI(current.baseURI);
+      var base = this.parseURI(current.baseURI);
+      //console.log("Processing: "+this.ancestorPath(current)+", "+base.spec);
 
       // handle loading profiles (@profile)
       var profileAtt = current.getAttributeNode("profile");
@@ -243,11 +384,11 @@ RDFaProcessor.prototype.process = function(node) {
          // establish new subject and value
          var aboutAtt = current.getAttributeNode("about");
          if (aboutAtt) {
-            newSubject = this.curieParser.parseCURIEOrURI(aboutAtt.value,prefixes,base);
+            newSubject = this.parseCURIEOrURI(aboutAtt.value,prefixes,base);
          } else {
             var srcAtt = current.getAttributeNode("src");
             if (srcAtt) {
-               newSubject = base.resolve(srcAtt.value).spec;
+               newSubject = base.resolve(srcAtt.value);
             }
          }
          if (!newSubject) {
@@ -265,30 +406,30 @@ RDFaProcessor.prototype.process = function(node) {
          }
          var resourceAtt = current.getAttributeNode("resource");
          if (resourceAtt) {
-            currentObjectResource = this.curieParser.parseCURIEOrURI(resourceAtt.value,prefixes,base);
+            currentObjectResource = this.parseCURIEOrURI(resourceAtt.value,prefixes,base);
          } else {
             var hrefAtt = current.getAttributeNode("href");
             if (hrefAtt) {
-               currentObjectResource = base.resolve(hrefAtt.value).spec;
+               currentObjectResource = base.resolve(hrefAtt.value);
             }
          }
       } else {
          // establish new subject
          var aboutAtt = current.getAttributeNode("about");
          if (aboutAtt) {
-            newSubject = this.curieParser.parseCURIEOrURI(aboutAtt.value,prefixes,base);
+            newSubject = this.parseCURIEOrURI(aboutAtt.value,prefixes,base);
          } else {
             var srcAtt = current.getAttributeNode("src");
             if (srcAtt) {
-               newSubject = base.resolve(srcAtt.value).spec;
+               newSubject = base.resolve(srcAtt.value);
             } else {
                var resourceAtt = current.getAttributeNode("resource");
                if (resourceAtt) {
-                  newSubject = this.curieParser.parseCURIEOrURI(resourceAtt.value,prefixes,base);
+                  newSubject = this.parseCURIEOrURI(resourceAtt.value,prefixes,base);
                } else {
                   var hrefAtt = current.getAttributeNode("href");
                   if (hrefAtt) {
-                     newSubject = base.resolve(hrefAtt.value).spec;
+                     newSubject = base.resolve(hrefAtt.value);
                   }
                }
             }
@@ -312,7 +453,7 @@ RDFaProcessor.prototype.process = function(node) {
          }
       }
 
-      //alert(current.tagName+": newSubject="+newSubject+", currentObjectResource="+currentObjectResource+", skip="+skip);
+      //console.log(current.tagName+": newSubject="+newSubject+", currentObjectResource="+currentObjectResource+", skip="+skip);
 
       if (newSubject) {
          var origins = this.target.subjectOrigins[newSubject];
@@ -327,7 +468,7 @@ RDFaProcessor.prototype.process = function(node) {
       if (newSubject && typeofAtt) {
          var values = this.tokenize(typeofAtt.value);
          for (var i=0; i<values.length; i++) {
-            var object = this.curieParser.parseTermOrCURIEOrURI(values[i],terms,prefixes,base);
+            var object = this.parseTermOrCURIEOrURI(values[i],terms,prefixes,base);
             if (object) {
                this.addTriple(this.target,current,newSubject,this.typeURI,{ type: this.objectURI , value: object});
             }
@@ -340,7 +481,7 @@ RDFaProcessor.prototype.process = function(node) {
             var values = this.tokenize(relAtt.value);
             //alert(newSubject+" "+relAtt.value+" "+currentObjectResource+" "+values.length);
             for (var i=0; i<values.length; i++) {
-               var predicate = this.curieParser.parseTermOrCURIEOrURI(values[i],terms,prefixes,base);
+               var predicate = this.parseTermOrCURIEOrURI(values[i],terms,prefixes,base);
                if (predicate) {
                   this.addTriple(this.target,current,newSubject,predicate,{ type: this.objectURI, value: currentObjectResource});
                }
@@ -349,7 +490,7 @@ RDFaProcessor.prototype.process = function(node) {
          if (revAtt) {
             var values = this.tokenize(revAtt.value);
             for (var i=0; i<values.length; i++) {
-               var predicate = this.curieParser.parseTermOrCURIEOrURI(values[i],terms,prefixes,base);
+               var predicate = this.parseTermOrCURIEOrURI(values[i],terms,prefixes,base);
                if (predicate) {
                   this.addTriple(this.target,current,currentObjectResource, predicate, { type: this.objectURI, value: newSubject});
                }
@@ -365,7 +506,7 @@ RDFaProcessor.prototype.process = function(node) {
          if (relAtt) {
             var values = this.tokenize(relAtt.value);
             for (var i=0; i<values.length; i++) {
-               var predicate = this.curieParser.parseTermOrCURIEOrURI(values[i],terms,prefixes,base);
+               var predicate = this.parseTermOrCURIEOrURI(values[i],terms,prefixes,base);
                if (predicate) {
                   incomplete.push({ predicate: predicate, forward: true });
                }
@@ -375,7 +516,7 @@ RDFaProcessor.prototype.process = function(node) {
          if (revAtt) {
             var values = this.tokenize(revAtt.value);
             for (var i=0; i<values.length; i++) {
-               var predicate = this.curieParser.parseTermOrCURIEOrURI(values[i],terms,prefixes,base);
+               var predicate = this.parseTermOrCURIEOrURI(values[i],terms,prefixes,base);
                if (predicate) {
                   incomplete.push({ predicate: predicate, forward: false });
                }
@@ -389,12 +530,12 @@ RDFaProcessor.prototype.process = function(node) {
          var contentAtt = current.getAttributeNode("content");
          var datatype = null;
          if (datatypeAtt) {
-            datatype = this.curieParser.parseTermOrCURIEOrURI(datatypeAtt.value,terms,prefixes,base);
+            datatype = this.parseTermOrCURIEOrURI(datatypeAtt.value,terms,prefixes,base);
          }
          var values = this.tokenize(propertyAtt.value);
          var content = datatype==this.XMLLiteralURI ? null : (contentAtt ? contentAtt.value : current.textContent); 
          for (var i=0; i<values.length; i++) {
-            var predicate = this.curieParser.parseTermOrCURIEOrURI(values[i],terms,prefixes,base);
+            var predicate = this.parseTermOrCURIEOrURI(values[i],terms,prefixes,base);
             if (predicate) {
                if (datatype==this.XMLLiteralURI) {
                   this.addTriple(this.target,current,newSubject,predicate,{ type: this.XMLLiteralURI, value: current.childNodes});
@@ -409,7 +550,7 @@ RDFaProcessor.prototype.process = function(node) {
       var childContext = null;
       if (skip) {
          // TODO: should subject be null?
-         childContext = this.push(context,base,context.subject);
+         childContext = this.push(context,context.subject);
          // TODO: should the parentObject be passed along?  If not, then intermediary children will keep properties from being associated with incomplete triples.
          // TODO: Verify: if the current baseURI has changed and the parentObject is the parent's base URI, then the baseURI should change
          childContext.parentObject = current.parentNode.baseURI==context.parentObject ? current.baseURI : context.parentObject;
@@ -429,7 +570,7 @@ RDFaProcessor.prototype.process = function(node) {
                }
             }
          }
-         childContext = this.push(context,base,newSubject);
+         childContext = this.push(context,newSubject);
          childContext.parentObject = currentObjectResource ? currentObjectResource : (newSubject ? newSubject : context.subject);
          childContext.prefixes = prefixes;
          childContext.incomplete = incomplete;
@@ -446,10 +587,9 @@ RDFaProcessor.prototype.process = function(node) {
 }
 
 
-RDFaProcessor.prototype.push = function(parent,base,subject) {
+RDFaProcessor.prototype.push = function(parent,subject) {
    return {
       parent: parent,
-      base: base ? base : (parent ? parent.base : null),
       subject: subject ? subject : (parent ? parent.subject : null),
       parentObject: null,
       incomplete: [],
