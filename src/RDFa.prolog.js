47ca40f42eeb40ee569e270223a2136ead353244
alex@milowski.com <alex@milowski.com@0d82c3c9-fcea-40e7-27d5-4b35ae388c16>
2013-01-11 17:50:37 +0000

    removed the update flag added an experimental track changes feature

--- src/RDFa.prolog.js
+++ src/RDFa.prolog.js
@@ -1,4 +1,23 @@
-function RDFa() {
+var RDFa = RDFa || {};
+
+RDFa.trackChanges = function(flag) {
+   if (flag) {
+      if (RDFa.observer) { return; }
+      var observerConstructor = window.MutationObserver || window.WebKitMutationObserver || window.MozMutationObserver;
+      if (RDFa.trackChanges && observerConstructor) {
+         var changeObserved = function(mutations) {
+            console.log("document changed.");
+         };
+         RDFa.observer = observerConstructor(changeObserved);
+         RDFa.observer.observe(document.documentElement,{childList: true,attributes:true,characterData: true,subtree:true,
+            attributeFilter: ["typeof","rel","property","content","resource","href"] });
+      }
+   } else if (RDFa.observer) {
+      RDFa.observer.disconnect();
+      RDFa.observer = false;
+   }
+
 }
-RDFa.attach = function(document,update) {
+
+RDFa.attach = function(document) {
 
