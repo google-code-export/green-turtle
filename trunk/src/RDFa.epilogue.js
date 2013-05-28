4e7cd94c0e10d4204c99e7fef57ffb7c4bb09efc
alex@milowski.com <alex@milowski.com@0d82c3c9-fcea-40e7-27d5-4b35ae388c16>
2013-05-28 22:51:21 +0000

    Added processors implemented handling of scripts moved attach to impl interface

--- trunk/src/RDFa.epilogue.js
+++ trunk/src/RDFa.epilogue.js
@@ -1,98 +1,106 @@
-env.attach = function(document,options) {
+var implementation = {
 
-var hasFeature = document.implementation.hasFeature;
-document.implementation.hasFeature = function(feature,version) {
-   if (feature=="RDFaAPI" && version=="1.1") { return true; }
-   return hasFeature.apply(this,arguments);
-}
-
-var loaded = document.data ? true : false;
-if (document.head) {
-   var meta = document.createElement("meta");
-   meta.setAttribute("name","green-turtle-rdfa-message");
-   document.head.appendChild(meta);
-   var makeEvent = function() {
-      var event = document.createEvent("Event");
-      event.initEvent("green-turtle-rdfa-extension",true,true);
-      return event;
-   }
-   meta.addEventListener("green-turtle-rdfa-document",function(event) {
-      var message = JSON.parse(meta.getAttribute("content"));
-      if (message.type=="status") {
-         var doneEvent = makeEvent();
-         meta.setAttribute("content",'{ "type": "status", "loaded": '+loaded+', "count": '+document.data._data_.tripleCount+' }');
-         setTimeout(function() { meta.dispatchEvent(doneEvent); },1);
-      } else if (message.type=="get-subjects") {
-         var subjects = document.data.getSubjects();
-         meta.setAttribute("content",'{ "type": "subjects", "subjects": '+JSON.stringify(subjects)+' }');
-         var getSubjectsEvent = makeEvent();
-         setTimeout(function() { meta.dispatchEvent(getSubjectsEvent); },1);
-      } else if (message.type=="get-subject") {
-         var triples = null;
-         if (document.data.getSubject) {
-            // Use the Green Turtle triples extension
-            triples = document.data.getSubject(message.subject).toObject();
-         } else {
-            // Do it the hard way!
-            triples = { subject: message.subject, predicates: {} };
-            var projection = document.data.getProjection(message.subject);
-            var properties = projection.getProperties();
-            for (var i=0; i<properties.length; i++) {
-               var objects = [];
-               triples.predicates[properties[i]] = { predicate: predicate, objects: objects };
-               var values = projection.getAll(properties[i]);
-               for (var j=0; j<values.length; j++) {
-                  if (subjects[values[j]]) {
-                     objects.push({type: "http://www.w3.org/1999/02/22-rdf-syntax-ns#object",value: values[j]});
-                  } else {
-                     objects.push({ type:  "http://www.w3.org/1999/02/22-rdf-syntax-ns#PlainLiteral", value: values[j]});
+   attach: function(document,options) {
+      var hasFeature = document.implementation.hasFeature;
+      document.implementation.hasFeature = function(feature,version) {
+         if (feature=="RDFaAPI" && version=="1.1") { return true; }
+         return hasFeature.apply(this,arguments);
+      }
+      
+      var loaded = document.data ? true : false;
+      if (document.head) {
+         var meta = document.createElement("meta");
+         meta.setAttribute("name","green-turtle-rdfa-message");
+         document.head.appendChild(meta);
+         var makeEvent = function() {
+            var event = document.createEvent("Event");
+            event.initEvent("green-turtle-rdfa-extension",true,true);
+            return event;
+         }
+         meta.addEventListener("green-turtle-rdfa-document",function(event) {
+            var message = JSON.parse(meta.getAttribute("content"));
+            if (message.type=="status") {
+               var doneEvent = makeEvent();
+               meta.setAttribute("content",'{ "type": "status", "loaded": '+loaded+', "count": '+document.data._data_.tripleCount+' }');
+               setTimeout(function() { meta.dispatchEvent(doneEvent); },1);
+            } else if (message.type=="get-subjects") {
+               var subjects = document.data.getSubjects();
+               meta.setAttribute("content",'{ "type": "subjects", "subjects": '+JSON.stringify(subjects)+' }');
+               var getSubjectsEvent = makeEvent();
+               setTimeout(function() { meta.dispatchEvent(getSubjectsEvent); },1);
+            } else if (message.type=="get-subject") {
+               var triples = null;
+               if (document.data.getSubject) {
+                  // Use the Green Turtle triples extension
+                  triples = document.data.getSubject(message.subject).toObject();
+               } else {
+                  // Do it the hard way!
+                  triples = { subject: message.subject, predicates: {} };
+                  var projection = document.data.getProjection(message.subject);
+                  var properties = projection.getProperties();
+                  for (var i=0; i<properties.length; i++) {
+                     var objects = [];
+                     triples.predicates[properties[i]] = { predicate: predicate, objects: objects };
+                     var values = projection.getAll(properties[i]);
+                     for (var j=0; j<values.length; j++) {
+                        if (subjects[values[j]]) {
+                           objects.push({type: "http://www.w3.org/1999/02/22-rdf-syntax-ns#object",value: values[j]});
+                        } else {
+                           objects.push({ type:  "http://www.w3.org/1999/02/22-rdf-syntax-ns#PlainLiteral", value: values[j]});
+                        }
+                     }
                   }
                }
+               var response = { type: "subject", subject: message.subject, triples: triples };
+               meta.setAttribute("content",JSON.stringify(response));
+               var getSubjectEvent = makeEvent();
+               setTimeout(function() { meta.dispatchEvent(getSubjectEvent); },1);
             }
-         }
-         var response = { type: "subject", subject: message.subject, triples: triples };
-         meta.setAttribute("content",JSON.stringify(response));
-         var getSubjectEvent = makeEvent();
-         setTimeout(function() { meta.dispatchEvent(getSubjectEvent); },1);
+         },false);
       }
-   },false);
-}
-
-
-if (!document.data) {
-   DocumentData.attach(document,options);
-}
-
-var processDoc = function() {
-   var processor = new GraphRDFaProcessor(document.data._data_);
-   processor.finishedHandlers.push(function(node) {
-      if (node.ownerDocument) {
-         var event = node.ownerDocument.createEvent("HTMLEvents");
-         event.initEvent("rdfa.loaded",true,true);
-         node.ownerDocument.dispatchEvent(event);
+      
+      
+      if (!document.data) {
+         DocumentData.attach(document,options);
       }
-   });
-   processor.process(document.documentElement,options);
-   loaded = true;
-   
-}
+      
+      var processDoc = function() {
+         var processor = new GraphRDFaProcessor(document.data._data_);
+         // Note: This handler MUST execute before the rdfa.loaded event is sent
+         processor.finishedHandlers.push(function(node) {
+            for (var mediaType in implementation.processors) {
+               var processor = implementation.processors[mediaType];
+               processor.process(document,options);
+            }
+         });
+         processor.finishedHandlers.push(function(node) {
+            if (node.ownerDocument) {
+               var event = node.ownerDocument.createEvent("HTMLEvents");
+               event.initEvent("rdfa.loaded",true,true);
+               node.ownerDocument.dispatchEvent(event);
+            }
+         });
+         processor.process(document.documentElement,options);
+         loaded = true;
+         
+      }
+      
+      if (document.readyState=="loading") {
+         window.addEventListener("load",function() {
+            processDoc();
+         },false);
+      } else {
+         processDoc();
+      }
+      
+   },
 
-if (document.readyState=="loading") {
-   window.addEventListener("load",function() {
-      processDoc();
-   },false);
-} else {
-   processDoc();
-}
-
-};
-
-var implementation = {
    parse: function(text,mediaType,options) {
-      if (mediaType!="text/turtle") {
+      var parserFactory = this.processors[mediaType];
+      if (!parserFactory) {
          throw "Unsupported media type "+mediaType;
       }
-      var parser = new TurtleParser();
+      var parser = parserFactory.createParser();
       if (options && options.errorHandler) {
          parser.onError = options.errorHandler;
       }
@@ -107,6 +115,45 @@ var implementation = {
    createDocumentData: function(baseURI) {
       return new DocumentData(baseURI ? baseURI : window ? window.location.href : "about:blank");
    }
+   
+};
+
+Object.defineProperty(implementation,"processors", {
+   value: {},
+   writable: false,
+   configurable: false,
+   enumerable: true
+});
+
+implementation.processors["text/turtle"] = {
+   createParser: function() {
+      return new TurtleParser();
+   },
+   process: function(node,options) {
+      var owner = node.nodeType==Node.DOCUMENT_NODE ? node : node.ownerDocument;
+      if (!owner.data) {
+         return;
+      }
+      var success = true;
+      var scripts = owner.getElementsByTagNameNS("http://www.w3.org/1999/xhtml","script");
+      for (var i=0; i<scripts.length; i++) {
+         if (scripts[i].getAttribute("type")!="text/turtle") {
+            continue;
+         }
+         var parser = this.createParser();
+         if (options && options.errorHandler) {
+            parser.onError = options.errorHandler;
+         }
+         var base = options ? options.baseURI : null;
+         parser.parse(scripts[i].textContent,base);
+         if (parser.errorCount>0) {
+            success = false;
+         } else {
+            owner.data.merge(parser.context.subjects,{ prefixes: parser.context.prefixes});
+         }
+      }
+      return success;
+   }
 };
 
 Object.defineProperty(env,"implementation", {
@@ -115,6 +162,10 @@ Object.defineProperty(env,"implementation", {
    configurable: false,
    enumerable: true
 });
+
+env.attach = function(document,options) {
+   implementation.attach(document,options);
+};
 
 return env;
 
